e36ec50e94c0f2794acbbfa1718a864b46088e6e
Max4297 <levanov.max4297@gmail.com>
2023-09-13 23:58:58 +0300

    Completed the slider in the category section

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,35 @@
 $(function () {
-  $(".category__slider-inner").slick({
+  $(".category__slider-typeFood").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     dots: true,
     autoplay: false,
     prevArrow:
-      '<button type="button" class="slick-prev"><img class="svg-roundarrow-left-dims" src="../images/sprite.svg#roundarrow-left" alt="arrow left"></button>',
+      '<button type="button" class="slick-prev"><svg class="svg-roundarrow-left-dims"><use xlink:href="../images/sprite.svg#roundarrow-left"></svg></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img class="svg-roundarrow-right-dims" src="../images/sprite.svg#roundarrow-right" alt="arrow right"></button>',
+      '<button type="button" class="slick-next"><svg class="svg-roundarrow-right-dims"><use xlink:href="../images/sprite.svg#roundarrow-right"></svg></button>',
   });
 });
+
+let categories = document.querySelectorAll(".category__buttons-item");
+let sliders = document.querySelectorAll(".category__slider-typeFood");
+
+for (const elem of categories) {
+  elem.addEventListener("click", function () {
+    for (const elem1 of categories) {
+      elem1.classList.remove("category__buttons-item--active");
+    }
+    this.classList.add("category__buttons-item--active");
+
+    console.log(sliders);
+
+    for (const elem2 of sliders) {
+      elem2.classList.remove("category__slider-typeFood--active");
+    }
+
+    sliders[elem.dataset.category].classList.add(
+      "category__slider-typeFood--active"
+    );
+  });
+}
